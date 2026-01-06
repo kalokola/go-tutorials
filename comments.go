f61b2c8b804860e66eb4ef59800cf81834823d3f
kalokola <brightiuskalokola@gmail.com>
2026-01-05 18:51:38 -0800

    first commit

---
+++ comments.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+
+func main(){
+	// a function to print a full name of brightius kalokola
+	/* this is a mutiline comment 
+	you can adjusts and place more contents here
+	*/
+	firstName := "Brightius";
+	lastName := "Kalokola";
+	_, words := fmt.Printf("My name is  %v %v", firstName, lastName)
+	fmt.Println(words)
+}
